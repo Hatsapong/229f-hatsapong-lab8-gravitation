6d63b2d7703d72673947cbd21910a209026962f6
Hatsapong <hatsapong.api@gmail.com>
2025-03-13 16:08:41 +0700

    Apply Gravity

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,16 +1,48 @@
+using NUnit.Framework;
+using System.Collections.Generic; //for List
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+
+    const float G = 0.00667f;
+
+    public static List<Gravity> gravityObjectList;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+            gravityObjectList = new List<Gravity>();
+
+
+        gravityObjectList.Add( this );
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        foreach (var obj in gravityObjectList)
+        {
+            //call Attract
+            if (obj != this)
+            Attract(obj);
+        }
         
+
     }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * otherRb.mass/ Mathf.Pow( distance, 2));
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(gravityForce);
+    }
+
 }
